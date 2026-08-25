98331f99e5c5290f965d1b7ccc44786f7d131260
MAPENZI-MINANI-JOSAPHAT <mapenzi.minani@ucbukavu.ac.cd>
2026-08-25 23:57:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,4 +12,4 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 async def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(request=request, name="index.html")
